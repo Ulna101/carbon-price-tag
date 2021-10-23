779a0f859a4af7d60eb40f8d06b57d6b6bd86a7c
Raymond Dai <raymond_dai@brown.edu>
2021-10-23 16:58:09 -0400

    modified settings

--- carbon_price_tag/settings.py
+++ carbon_price_tag/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-3i-o!m_ov9bhzmo98x8*pb#0qh8n8w*%#zl7@pvcx02w&(j#xz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
