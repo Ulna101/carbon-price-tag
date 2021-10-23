f0e45d4bfaecae6dafd2da6d7c75416231b166bb
Raymond Dai <raymond_dai@brown.edu>
2021-10-23 17:14:24 -0400

    rearrange webpage

--- carbon_price_tag/urls.py
+++ carbon_price_tag/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+import carbon_price_qr.views
+
 urlpatterns = [
-    path('carbon_price_qr', include('carbon_price_qr.urls')),
+    path("", carbon_price_qr.views.index, name="index"),
     path('admin/', admin.site.urls)
 ]
